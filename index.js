b1152e1e549aa522eb158639f7afd8594fd6964c
Paulo Pinto <gomescasador@gmail.com>
2023-04-26 18:50:11 +0100

    Modificação de teste para fluxo

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (request, response) => {
-    return response.json({message: 'Server is up'})
+    return response.json({message: 'Server is up!!!'})
 })
 
 app.listen(3333)
